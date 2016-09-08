2f77d42626d2a87c214bbabf9f3cc27e49529acb
Justin Musgrove <JTMusky@gmail.com>
2016-09-07 19:19:11 -0500

    Wrote out goals

--- ASM-Helper.bat
+++ ASM-Helper.bat
@@ -1,9 +1,61 @@
+::ORG       N/A
+::Copyright Unlicense
+::NAME      ASM-Helper.bat
+::PURPOSE   Expedite helper for Reverse Engineering Command.
+::AUTHOR    Justin Musgrove <jtmusky@gmail.com>
+::Revision  00.00.01 20160513
+
 @ECHO OFF
 SETLOCAL
 TITLE ASM Decompiler Helper
 GOTO :EndComments
 
+Revision:   00.00.01 20160907 - JM: Initial Version
+                - Empty shell
 
+GOALS KEY:  X Complete, C CANX, M Moved, p particial
+PRIORITY:   1 HIGH, 2 MED, 3 LOW
+
+GOALS:      - Sanity Checks: Ensure program will run gooooood
+                3:[ ] Windows Version check, WinXP, Win7
+                2:[ ] Detect between dir and file, run different
+                1:[ ] Ensure gracefull, or glorious crashes
+
+            - Tool choice for user
+                1:[ ] Prompt user for type of output
+
+            - Working area
+                1:[ ] Desktop working folder based on time
+                1:[ ] Copy original file with pre-fix "MOD-"
+                2:[ ] Verbose debug output of the file
+                2:[ ] Record information of the target file
+
+
+LICENSE:
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org>
 
 :EndComments
 
